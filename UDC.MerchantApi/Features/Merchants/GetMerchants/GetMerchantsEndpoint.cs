0810caefce3200ed49b899109d21318ee94fba20
leveebreaks <83450119+leveebreaks@users.noreply.github.com>
2025-06-16 22:51:17 +0300

    name and category basic filtering

--- UDC.MerchantApi/Features/Merchants/GetMerchants/GetMerchantsEndpoint.cs
+++ UDC.MerchantApi/Features/Merchants/GetMerchants/GetMerchantsEndpoint.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using UDC.MerchantApi.Domain;
 using UDC.MerchantApi.Infrastructure.Persistance;
 
 namespace UDC.MerchantApi.Features.Merchants.GetMerchants;
@@ -8,9 +10,19 @@ public static class GetMerchantsEndpoint
 {
     public static RouteGroupBuilder MapGetMerchants(this RouteGroupBuilder routeGroup)
     {
-        routeGroup.MapGet("/", async (string category, string name, AppDbContext db, IMapper mapper) =>
+        routeGroup.MapGet("/", async (string name, string category, AppDbContext db, IMapper mapper) =>
         {
-            var merchants = await db.Merchants.Select(x => mapper.Map<MerchantDto>(x)).ToListAsync();
+            IQueryable<Merchant> query = db.Merchants;
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                query = query.Where(m => m.Name == name);
+            } 
+            else if (!string.IsNullOrWhiteSpace(category))
+            {
+                Enum.TryParse(category, true, out Category categoryEnum);
+                query = query.Where(m => m.Category == categoryEnum);
+            }
+            var merchants = await query.Select(x => mapper.Map<MerchantDto>(x)).ToListAsync();
             return Results.Ok(merchants);
         });
         
