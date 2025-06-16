c9447b10d1eff24cbda353a8d3cbb444decfad98
leveebreaks <83450119+leveebreaks@users.noreply.github.com>
2025-06-16 14:34:19 +0300

    initial dbcontext, entity and migrations

---
+++ UDC.MerchantApi/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UDC.MerchantApi.Domain;
+
+namespace UDC.MerchantApi.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Merchant> Merchants { get; set; }
+    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Merchant>(e =>
+        {
+            e.Property(m => m.Name).IsRequired().HasMaxLength(100);
+            e.Property(m => m.Email).IsRequired();
+            e.Property(m => m.Category).IsRequired();
+            e.Property(m => m.CreatedAt).HasDefaultValueSql("GetUtcDate()");
+        });
+    }
+}
---
+++ UDC.MerchantApi/Domain/Merchant.cs
@@ -0,0 +1,10 @@
+﻿namespace UDC.MerchantApi.Domain;
+
+public class Merchant
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Category { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
